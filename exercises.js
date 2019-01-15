666ecbf2d4ce7d4339750b488e27534f07581ad0
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-01-14 21:29:09 -1000

    finished Q#8

--- exercises.js
+++ exercises.js
@@ -131,6 +131,10 @@ nope
 turn
 */
 
+for (i=oopsArray.length-1;i>=0;i--) {
+   console.log(oopsArray[i]);
+}
+
 
 /* 9) Siesta Time
 Declare a variable named `napSchedule` and assign its value to the following array: `[false, false, true, false, true, true]`
